97fa6cfc33a1b2335f75973d2bf750dd1f693af2
manisha guda <manishavguda@gmail.com>
2023-07-12 20:52:03 +0530

    terraform

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_vpc" "main" {
 
 }
 
-resource "aws_subnets" "main" {
+resource "aws_subnet" "main" {
   count  = length(var.subnets_cidr)
   vpc_id = aws_vpc.main.id
   cidr_block = var.subnets_cidr[count.index]
