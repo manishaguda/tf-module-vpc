e20f8cdaf3364e9e9e1b3bee74da0dff72e45420
manisha guda <manishavguda@gmail.com>
2023-07-13 18:37:26 +0530

    terraform

--- main.tf
+++ main.tf
@@ -23,4 +23,5 @@ resource "aws_vpc_peering_connection" "peer" {
   peer_owner_id = data.aws_caller_identity.current.account_id
   peer_vpc_id   = vpc-03d4e63006cb172ca
   vpc_id        = aws_vpc.main.id
+  auto_accept               = true
 }
