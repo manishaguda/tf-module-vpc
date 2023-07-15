dcb23c9bd34ac261c681dd9229af2dca67401542
manisha guda <manishavguda@gmail.com>
2023-07-15 12:10:35 +0530

    terraform

--- main.tf
+++ main.tf
@@ -54,11 +54,31 @@ resource "aws_internet_gateway" "igw" {
 
 }
 
-resource "aws_route_table_association" "public-rt-assoc" {
-  count          = length(aws_subnet.public)
-  vpc_id         = aws_subnet.public.*.id[count.index]
-  route_table_id = aws_route_table.public.id
+resource "aws_default_route_table" "public" {
+  default_route_table_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  route {
+    cidr_block = data.aws_vpc.default.cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+  }
+
+  tags       = merge(
+    local.common_tags,
+    { Name = "${var.env}-public-route-table" }
+  )
+
 }
+
+#resource "aws_route_table_association" "public-rt-assoc" {
+#  count          = length(aws_subnet.public)
+#  vpc_id         = aws_subnet.public.*.id[count.index]
+#  route_table_id = aws_route_table.public.id
+#}
 
 # Create ec2
 #
