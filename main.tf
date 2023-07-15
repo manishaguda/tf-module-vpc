82f161d75c5683cfcf478d2ef7fa8f76ae98beff
manisha guda <manishavguda@gmail.com>
2023-07-15 11:10:02 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_vpc" "main" {
   cidr_block = var.cidr_block
   tags       = merge(
-      local.common_tags,
-      { Name = "${var.env}-vpc" }
-     )
+    local.common_tags,
+    { Name = "${var.env}-vpc" }
+  )
 
 }
 
