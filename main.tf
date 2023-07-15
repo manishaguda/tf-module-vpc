292234bf878663abcf944db005aac9c4d42ba1fc
manisha guda <manishavguda@gmail.com>
2023-07-15 12:20:03 +0530

    terraform

--- main.tf
+++ main.tf
@@ -76,7 +76,7 @@ resource "aws_route_table" "public" {
 
 resource "aws_route_table_association" "public-rt-assoc" {
   count          = length(aws_subnet.public)
-  vpc_id         = aws_subnet.public.*.id[count.index]
+  subnet_id      = aws_subnet.public.*.id[count.index]
   route_table_id = aws_route_table.public.id
 }
 
