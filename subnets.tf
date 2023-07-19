29eb8c48cf6a07f98a964ef979d8f327e04b99e4
manisha guda <manishavguda@gmail.com>
2023-07-19 21:37:04 +0530

    terraform

--- subnets.tf
+++ subnets.tf
@@ -7,9 +7,12 @@ module "subnets" {
 
   for_each   = var.subnets
   cidr_block = each.value.cidr_block
-  name       = each.value.name
+
+
+  name       = each.value
 
   vpc_id                    = aws_vpc.main.id
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+
   tags                      = local.common_tags
 }
