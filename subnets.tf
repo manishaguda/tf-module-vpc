bbd2f47e3df54a8402cb87d9d02972b71a4fc686
manisha guda <manishavguda@gmail.com>
2023-07-28 12:46:12 +0530

    terraform

--- subnets.tf
+++ subnets.tf
@@ -6,14 +6,14 @@ module "public_subnets" {
 
   for_each    = var.public_subnets
   cidr_block  = each.value.cidr_block
-  name        = each.value.name
-  internet_gw = lookup(each.value, "internet_gw", false)
-  nat_gw      = lookup(each.value, "nat_gw", false)
-
-  vpc_id                    = aws_vpc.main.id
-  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-  tags                      = local.common_tags
-  gateway_id                = aws_internet_gateway.gw.id
+#  name        = each.value.name
+#  internet_gw = lookup(each.value, "internet_gw", false)
+#  nat_gw      = lookup(each.value, "nat_gw", false)
+#
+#  vpc_id                    = aws_vpc.main.id
+#  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+#  tags                      = local.common_tags
+#  gateway_id                = aws_internet_gateway.gw.id
 
 }
 
