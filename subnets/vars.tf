64e665004a6994e0040d3cd2465b46d3d19c98b0
manisha guda <manishavguda@gmail.com>
2023-07-29 10:21:21 +0530

    terraform

--- subnets/vars.tf
+++ subnets/vars.tf
@@ -1,6 +1,6 @@
-variable "cidr_block" {}
-variable "env" {}
-variable "default_vpc_id" {}
-#variable "subnets" {}
-variable "availability_zone" {}
+#variable "cidr_block" {}
+#variable "env" {}
+#variable "default_vpc_id" {}
+##variable "subnets" {}
+#variable "availability_zone" {}
 
