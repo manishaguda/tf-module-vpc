0e6283afbd3596df99c9c68d7896aca480832640
manisha guda <manishavguda@gmail.com>
2023-07-31 11:39:48 +0530

    terraform

--- outputs.tf
+++ outputs.tf
@@ -10,4 +10,6 @@ output "public_subnet_ids" {
   value = module.public_subnets
 }
 
-
+output "one_subnet_id" {
+  value = lookup(lookup(module.public_subnets, "public", null), "subnet_ids", null)[0]
+}
