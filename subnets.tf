bc0cfd888b6e28b12d47f5417906dd50dad61a31
manisha guda <manishavguda@gmail.com>
2023-07-31 13:56:24 +0530

    terraform

--- subnets.tf
+++ subnets.tf
@@ -8,7 +8,7 @@ module "public_subnets" {
   cidr_block  = each.value.cidr_block
   name        = each.value.name
   internet_gw = lookup(each.value, "internet_gw", false)
-#  nat_gw      = lookup(each.value, "nat_gw", false)
+  nat_gw      = lookup(each.value, "nat_gw", false)
 
   vpc_id                    = aws_vpc.main.id
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
@@ -18,20 +18,20 @@ module "public_subnets" {
 
 }
 
-#module "private_subnets" {
-#  source            = "./subnets"
-#  default_vpc_id    = var.default_vpc_id
-#  env               = var.env
-#  availability_zone = var.availability_zone
-#
-#  for_each          = var.private_subnets
-#  cidr_block        = each.value.cidr_block
-#  name              = each.value.name
-#  internet_gw       = lookup(each.value, "internet_gw", false)
-##  nat_gw            = lookup(each.value, "nat_gw", false)
-#
-#  vpc_id                    = aws_vpc.main.id
-#  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-#  tags                      = local.common_tags
-#  nat_gw_id                 = aws_nat_gateway.ngw.id
-#}
+module "private_subnets" {
+  source            = "./subnets"
+  default_vpc_id    = var.default_vpc_id
+  env               = var.env
+  availability_zone = var.availability_zone
+
+  for_each          = var.private_subnets
+  cidr_block        = each.value.cidr_block
+  name              = each.value.name
+  internet_gw       = lookup(each.value, "internet_gw", false)
+  nat_gw            = lookup(each.value, "nat_gw", false)
+
+  vpc_id                    = aws_vpc.main.id
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+  tags                      = local.common_tags
+  nat_gw_id                 = aws_nat_gateway.ngw.id
+}
