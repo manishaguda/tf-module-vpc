395c88a9e29b33e073ded66e9a201467e4888aa5
manisha guda <manishavguda@gmail.com>
2023-07-31 13:23:27 +0530

    terraform

--- main.tf
+++ main.tf
@@ -38,13 +38,13 @@ resource "aws_eip" "ngw-eip" {
   vpc = true
 }
 
-resource "aws_nat_gateway" "ngw" {
-  allocation_id = aws_eip.ngw-eip.id
-  subnet_id     = var.public_subnet_ids[0]
-
-  tags = merge(
-    local.common_tags,
-    { Name = "${var.env}-ngw" }
-  )
-
-}
+#resource "aws_nat_gateway" "ngw" {
+#  allocation_id = aws_eip.ngw-eip.id
+#  subnet_id     = var.public_subnet_ids[0]
+#
+#  tags = merge(
+#    local.common_tags,
+#    { Name = "${var.env}-ngw" }
+#  )
+#
+#}
