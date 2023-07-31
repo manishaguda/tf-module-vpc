a9a8358db58bb6e30aa8496b189faf24595f22ff
manisha guda <manishavguda@gmail.com>
2023-07-31 12:43:52 +0530

    terraform

--- subnets/main.tf
+++ subnets/main.tf
@@ -39,9 +39,9 @@ resource "aws_route" "internet_gw_route" {
   gateway_id             = var.gateway_id
 }
 
-resource "aws_route" "nat_gw_route" {
-  count                  = var.nat_gw ? 1 : 0
-  route_table_id         = aws_route_table.route_table.id
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id            = var.nat_gw_id
-}
+#resource "aws_route" "nat_gw_route" {
+#  count                  = var.nat_gw ? 1 : 0
+#  route_table_id         = aws_route_table.route_table.id
+#  destination_cidr_block = "0.0.0.0/0"
+#  nat_gateway_id            = var.nat_gw_id
+#}
